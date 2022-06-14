22cf656a42f7a9feaef450a0f349314c2c015b42
konstantin <nkp.yalta@mail.ru>
2022-06-14 14:19:48 +0300

    добавил exes3

---
+++ exes3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+int prompt(string message)
+{
+    System.Console.Write(message);
+    string number = Console.ReadLine();
+    int numberInt = int.Parse(number);
+    return numberInt;
+}
+int numberInt = prompt("Введите число : ");
+
+if (numberInt < 100) System.Console.WriteLine(("Третьей цыфры нет"));
+else
+{
+    while (numberInt > 999)
+    {
+        numberInt = numberInt / 10;
+    }
+    numberInt = numberInt % 10;
+    System.Console.WriteLine(("Трутья цыфра числа" + numberInt));
+}
