ba44b2687aac2eeaf93793bdace5e9b568a6ffbb
konstantin <nkp.yalta@mail.ru>
2022-06-14 13:41:29 +0300

    добавил exes2

---
+++ exes2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine("Случайное рандомное число : " + number);
+
+int result = (number / 100) * 10 + number % 10;
+
+System.Console.WriteLine("Чило без второго символа : " + result);
