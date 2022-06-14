eb4f45e8fe801c8bd8638fcebdcbd1e07e62c9d8
konstantin <nkp.yalta@mail.ru>
2022-06-14 13:20:50 +0300

    добавил exes1

---
+++ exes1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.Write("Введите трёхзначное число : ");
+string paramString = Console.ReadLine();
+int paramInt = int.Parse(paramString);
+
+if (paramInt >1000 || paramInt<99)
+{
+    System.Console.WriteLine("Введене не верное число");
+}
+else 
+{
+    System.Console.WriteLine(" Вторая цифра : " + (paramInt % 100)/10);
+}
