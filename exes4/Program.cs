9886f7b886ef23cd2410b405f9ab3aded3904120
konstantin <nkp.yalta@mail.ru>
2022-06-14 17:16:48 +0300

    добавил exes4

---
+++ exes4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int prompt(string message)
+{
+    System.Console.Write(message);
+    string number = Console.ReadLine();
+    int numberInt = int.Parse(number);
+    return numberInt;
+}
+int numberInt = prompt("Введите цифру дня недели :  ");
+
+if (numberInt < 1 || numberInt > 7)
+{
+    System.Console.WriteLine("Эта цифра не соответствует дню недели .");
+    Environment.Exit(0);
+}
+if (numberInt == 6 || numberInt == 7) System.Console.WriteLine("Это выходной день ! ");
+else System.Console.WriteLine("Это рабочий день .");
